803604dd2ba109eb91dc264847fd6630b8c11061
LEOCHUINTIWI <leochuin16@gmail.com>
2023-07-04 11:44:30 -0500

    web

--- leo.js
+++ leo.js
@@ -1,2 +1,44 @@
 
-alert("hola")
+alert("hola...");
+/* comentario de varias lineas */
+
+var codigo;
+let nombre;
+
+const dato="2023";
+
+var edad;
+edad=10
+
+if(edad > 18){
+    alert ("mayor de edad")
+}
+else{
+    alert("noo");
+}
+
+var contador;
+
+for(contador=0; contador < 20; contador++){
+console.log(contador);
+}
+
+function f_suma(n1, n2){
+    var suma;
+    suma = n1 +n2;
+    return suma;
+}
+
+var resultado;
+resultado = f_suma(20,5);
+
+alert("la suma es: " + resultado);
+
+function respuesta (){
+    
+    var  numero= docuemnt-getElementById("numero").value;
+
+    document.getElementById("resultado").innerHTML = "<h1 style='color: red >" + numero + "</h1>";
+
+
+}
